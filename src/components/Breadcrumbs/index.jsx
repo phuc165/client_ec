5867799520c2513ca756e89ea5f6a46ac3928275
phuc165 <hatanphuc3456789@gmail.com>
2025-05-26 13:09:11 +0700

    fix the breadcrumb to show product name instead id

--- src/components/Breadcrumbs/index.jsx
+++ src/components/Breadcrumbs/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router';
+import { useSelector } from 'react-redux';
 import styles from '../../styles/core/breadcrumbs.module.scss';
 import clsx from 'clsx';
 
@@ -7,16 +8,29 @@ const Breadcrumb = () => {
     const location = useLocation();
     const pathSegments = location.pathname.split('/').filter((segment) => segment !== '');
 
+    // Get product data from Redux store
+    const { singleProduct } = useSelector((state) => state.products);
+
+    const getDisplayName = (segment, index, segments) => {
+        // Check if this is a product detail page (pattern: /product/:id)
+        if (segments[index - 1] === 'product' && singleProduct && singleProduct._id === segment) {
+            return singleProduct.name || segment.charAt(0).toUpperCase() + segment.slice(1);
+        }
+
+        // Default formatting: capitalize first letter
+        return segment.charAt(0).toUpperCase() + segment.slice(1);
+    };
+
     const breadcrumbItems = pathSegments.map((segment, index) => {
         const path = `/${pathSegments.slice(0, index + 1).join('/')}`;
         const isLast = index === pathSegments.length - 1;
-        const displayName = segment.charAt(0).toUpperCase() + segment.slice(1);
+        const displayName = getDisplayName(segment, index, pathSegments);
 
         return (
             <li
                 key={index}
                 className={clsx(styles.breadcrumb__item, {
-                    [styles['breadcrumb__item--last']]: isLast, // Conditional class for the last item
+                    [styles['breadcrumb__item--last']]: isLast,
                 })}
             >
                 {!isLast ? (
